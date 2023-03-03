4ffb13fa1972c484c754fbc342f733a4da63124f
Joe Abou Serhal <80884927+joeabouserhal@users.noreply.github.com>
2023-03-03 19:17:08 +0200

    port change

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const usersRouter = require("./routes/users/users");
 
+const port = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
   // res (response) is the response we'll be sending out
   // req (request) is the request sent by the client
@@ -18,6 +20,6 @@ app.all("*", (req, res) => {
   res.status(404).send("404 not found");
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server listening on port 5000");
 });
